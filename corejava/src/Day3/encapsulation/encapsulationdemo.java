814963a0e897fafed883f0f3370afb8c25e5e9de
Preethika <preethimahesh1992@gmail.com>
2025-10-03 19:38:17 +0530

    day1-3

---
+++ corejava/src/Day3/encapsulation/encapsulationdemo.java
@@ -0,0 +1,13 @@
+package Day3.encapsulation;
+
+public class encapsulationdemo {
+	public static void main(String[] args) {
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+
+	}
+}
