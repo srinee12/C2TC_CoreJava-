ebf63eee020763bc8c05d284e643891cd8cad99e
srinee12 <srineebalsubramaniyan@gmail.com>
2025-10-27 15:01:59 +0530

    day 12-14

---
+++ corejava/src/com/ifet/dayfourteen/synchronization/InsufficientBalanceException.java
@@ -0,0 +1,11 @@
+package com.ifet.dayfourteen.synchronization;
+public class InsufficientBalanceException extends Exception {
+
+	public InsufficientBalanceException() {
+		super("Insufficient balance in your account");
+	}
+
+	public InsufficientBalanceException(String message) {
+		super(message);
+	}
+}
